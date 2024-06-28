46ded60c056b6be78ca8ac9c26c91c28cd44b46d
tomo-john <keririnkery.kery@gmail.com>
2024-06-28 17:36:19 +0900

    update: folders

--- src/Laravel9TaskList/app/Http/Controllers/TaskController.php
+++ src/Laravel9TaskList/app/Http/Controllers/TaskController.php
@@ -3,12 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Folder;
 
 class TaskController extends Controller
 {
-    // index メソッドを追加する
+    /**
+     * 【タスク一覧ページの表示機能】
+     *
+     * GET /folders/{id}/tasks
+     * @param int $id
+     * @return \Illuminate\View\View
+    */ 
     public function index()
     {
-        return "Hello John.";
+        $folders = Folder::all();
+
+        return view('tasks/index', [
+          'folders' => $folders,
+        ]); 
     }
 }
