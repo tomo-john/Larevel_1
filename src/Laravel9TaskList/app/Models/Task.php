32fdc9197fb90fd7d98fdd6ec0b5f51dd0b8a080
tomo-john <keririnkery.kery@gmail.com>
2024-07-08 10:45:18 +0900

    add: task

--- src/Laravel9TaskList/app/Models/Task.php
+++ src/Laravel9TaskList/app/Models/Task.php
@@ -8,4 +8,46 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+
+    /**
+     * ステータス（状態）定義
+     * 
+     */
+     const STATUS = [
+       1 => [ 'label' => '未着手', 'class' => 'label-danger' ],
+       2 => [ 'label' => '着手中', 'class' => 'label-info' ],
+       3 => [ 'label' => '完了', 'class' => '' ],
+     ];
+
+    /**
+     * ステータス（状態）ラベルのアクセサメソッド
+     * 
+     * @return string
+     */
+
+    public function getStatusLabelAttribute()
+    {
+      $status = $this->attributes['status'];
+
+      if (!isset(self::STATUS[$status])) {
+        return '';
+      }
+      return self::STATUS[$status]['label'];
+    }
+
+    /**
+     * 状態を表すHTMLクラスのアクセサメソッド
+     * 
+     * @return string
+     */
+    public function getStatusClassAttribute()
+    {
+      $status = $this->attributes['status'];
+
+      if (!isset(self::STATUS[$status])) {
+        return '';
+      }
+
+      return self::STATUS[$status]['class'];
+    }
 }
