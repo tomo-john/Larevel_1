0b349dd8244ed190c6b3a3a8e3aca5375f9ec169
tomo-john <keririnkery.kery@gmail.com>
2024-07-11 10:43:21 +0900

    add: delete_folder

--- src/Laravel9TaskList/resources/views/tasks/index.blade.php
+++ src/Laravel9TaskList/resources/views/tasks/index.blade.php
@@ -41,8 +41,8 @@
                                     {{ $folder->title }}
                                 </a>
                             </td>
-                            <td><a href="{{ route('folders.edit', ['id' => $folder->id]) }}"">編集</a></td>
-                            <td><a href="#">削除</a></td>
+                            <td><a href="{{ route('folders.edit', ['id' => $folder->id]) }}">編集</a></td>
+                            <td><a href="{{ route('folders.delete', ['id' => $folder->id]) }}">削除</a></td>
                         </tr>
                         @endforeach
                     </table>
