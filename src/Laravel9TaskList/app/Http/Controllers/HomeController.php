a43050ac9eec24f2b795c2a5dfda6b5a7fc5edf8
tomo-john <keririnkery.kery@gmail.com>
2024-07-16 09:45:47 +0900

    add: home

--- src/Laravel9TaskList/app/Http/Controllers/HomeController.php
+++ src/Laravel9TaskList/app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,17 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        /** @var App\Models\User **/
+        $user = Auth::user();
+
+        $folder = $user->folders()->first();
+
+        if (is_null($folder)) {
+            return view('home');
+        }
+
+        return redirect()->route('tasks.index', [
+            'id' => $folder->id,
+        ]);
     }
 }
